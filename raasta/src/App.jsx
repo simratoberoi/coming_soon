1df72c516c7a7faf8cf96ace035442967cfac785
Simrat Oberoi <simratoberoi2006@gmail.com>
2025-09-20 02:08:13 +0530

    first commit

---
+++ raasta/src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+import './App.css'
+import Img_grid from './images.jsx'
+
+function App() {
+  <Img_grid> </Img_grid>
+}
+
+export default App
---
+++ raasta/src/images.jsx
@@ -0,0 +1,40 @@
+function Img_grid(){
+    return (
+      <div class="gallery">
+        <img
+          src="https://images.unsplash.com/photo-1583705694065-d705b703cb56?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt=""/>
+        <img
+          src="https://images.unsplash.com/photo-1545126178-862cdb469409?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt=""/>
+        <img
+          src="https://images.unsplash.com/photo-1514222134-b57cbb8ce073?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt=""/>
+        <img
+          src="https://images.unsplash.com/photo-1496372412473-e8548ffd82bc?q=80&w=2214&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt=""/>
+        <img
+          src="https://images.unsplash.com/photo-1524228529766-4d7fe5dc55ca?q=80&w=1029&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt=""/>
+        <img
+          src="https://images.unsplash.com/photo-1465919292275-c60ba49da6ae?q=80&w=2111&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt=""/>
+        <img
+          src="https://images.unsplash.com/photo-1581321863389-ef7d7bfe4b75?q=80&w=988&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt=""/>
+        <img
+          src="https://images.unsplash.com/photo-1626231681172-0c54e184bea5?q=80&w=1035&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt=""/>
+        <img
+          src="https://images.unsplash.com/photo-1582510003544-4d00b7f74220?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt=""/>
+        <img
+          src="https://images.unsplash.com/photo-1642922835816-e2ac68db5c42?q=80&w=1674&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt=""/>
+        <img
+          src="https://images.unsplash.com/photo-1517373263199-baee103d2980?q=80&w=2083&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt=""/>
+      </div>
+    );
+}
+export default Img_grid
